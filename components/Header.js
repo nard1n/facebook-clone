865f7a669d4b2e15f02f1a730dacd9d8c6a9f43f
Nardin Lachowski <nardin@live.com>
2021-05-15 13:24:55 -0400

    created new react next app

---
+++ components/Header.js
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+        <header>
+            {/* left */}
+            <div>
+                <Image />
+            </div>
+            
+            {/* center */}
+
+            {/* right */}
+
+        </header>
+    )
+}
+
+export default Header
