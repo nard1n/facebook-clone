f690f9d9f2d2b7b64de56fe7a8d9c66c7ab76605
Nardin Lachowski <nardin@live.com>
2021-06-14 17:05:19 -0400

    added server side rendering of the session in the getserversideprops async function to check for user login session

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
+import {getSession} from "next-auth/client";
 import Head from 'next/head';
 import Header from '../components/Header';
+import Login from '../components/Login';
 
-export default function Home() {
+export default function Home({ session }) {
+  if(!session) return <Login />;
+  
   return (
     <div>
       <Head>
         <title>Facebook | NL</title>
       </Head>
-      
+
         <Header />
 
       <main>
@@ -17,4 +21,15 @@ export default function Home() {
       </main>
     </div>
   )
+}
+//to allow for server side rendering in nextjs app
+export async function getServerSideProps (context) {
+  //get user
+  const session = await getSession(context);
+
+  return{
+    props: {
+      session
+    }
+  }
 }
