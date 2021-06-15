0a2d1e651cb3a0574f4f69ef013333e3b7dc8848
Nardin Lachowski <nardin@live.com>
2021-06-14 22:26:39 -0400

    added sidebar and sidebarrow components

--- components/Header.js
+++ components/Header.js
@@ -60,7 +60,7 @@ function Header() {
                     layout="fixed"
                 />
 
-                <p className="whitespace-nowrap font-semibold pr-3">Nardin Lachowski</p>
+                <p className="whitespace-nowrap font-semibold pr-3">{session.user.name}</p>
                 <ViewGridIcon className="icon" />
                 <ChatIcon className="icon" />
                 <BellIcon className="icon" />
--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,21 @@ import {getSession} from "next-auth/client";
 import Head from 'next/head';
 import Header from '../components/Header';
 import Login from '../components/Login';
+import Sidebar from '../components/Sidebar';
 
 export default function Home({ session }) {
   if(!session) return <Login />;
   
   return (
-    <div>
+    <div className="h-screen bg-gray-100 overflow-hidden">
       <Head>
         <title>Facebook | NL</title>
       </Head>
 
         <Header />
 
-      <main>
-        {/* Sidebar */}
+      <main className="flex">
+        <Sidebar />
         {/* Feed */}
         {/* Widgets */}
       </main>
