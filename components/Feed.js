d6404fd2e18d30b768009fc2168347ed9912bfb1
Nardin Lachowski <nardin@live.com>
2021-06-16 21:15:12 -0400

    creating Posts feed component and importing it onto feed

--- components/Feed.js
+++ components/Feed.js
@@ -1,5 +1,6 @@
 import Stories from "./Stories";
 import InputBox from "./InputBox";
+import Posts from "./Posts";
 
 function Feed() {
     return (
@@ -7,7 +8,7 @@ function Feed() {
             <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
                 <Stories />
                 <InputBox />
-                {/* posts */}
+                <Posts />
             </div>
         </div>
     )
