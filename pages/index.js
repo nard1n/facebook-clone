e72f7736fb797e9ca72ab8e4fa150e891d48c2eb
Nardin Lachowski <nardin@live.com>
2021-06-22 10:34:33 -0400

    passing posts as props, saving past additions :

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import {getSession} from "next-auth/client";
+import {getSession, useSession} from "next-auth/client";
 import Head from 'next/head';
 import Header from '../components/Header';
 import Login from '../components/Login';
 import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed';
-import { useSession } from 'next-auth/client';
+import { db } from "../firebase";
 
 
-export default function Home({ session }) {
+export default function Home({ session, posts }) {
   if(!session) return <Login />;
   
   return (
@@ -20,7 +20,7 @@ export default function Home({ session }) {
 
       <main className="flex">
         <Sidebar />
-        <Feed />
+        <Feed posts={posts} />
         {/* Widgets */}
       </main>
     </div>
@@ -31,9 +31,17 @@ export async function getServerSideProps (context) {
   //get user
   const session = await getSession(context);
 
+  const posts = await db.collection("posts").orderBy("timestamp", "desc").get();
+
+  const docs = posts.docs.map((post) => ({
+    id: post.id,
+    ...post.data(),
+    timestamp: null,
+  }));
+
   return{
     props: {
-      session
-    }
-  }
+      session, posts: docs
+    },
+  };
 }
