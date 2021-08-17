679cc94e022691eebc1f10b02fd688b9f326edc5
Nardin Lachowski <nardin@live.com>
2021-08-17 15:58:52 -0400

    updated changes

--- firebase.js
+++ firebase.js
@@ -1,18 +1,19 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
 import "firebase/storage";
+import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBUSeVztecyKWuNl71ry3BEs7fHUbbk2Uo",
-    authDomain: "facebookpoi.firebaseapp.com",
-    projectId: "facebookpoi",
-    storageBucket: "facebookpoi.appspot.com",
-    messagingSenderId: "163833942561",
-    appId: "1:163833942561:web:ca0d20ddff1d4b5ff34960",
-  };
+  apiKey: "AIzaSyCgZjMmt6Wxs1_OdbEBzto3xDpnzcTiIpI",
+  authDomain: "facebook-clone-nl.firebaseapp.com",
+  projectId: "facebook-clone-nl",
+  storageBucket: "facebook-clone-nl.appspot.com",
+  messagingSenderId: "1073100223774",
+  appId: "1:1073100223774:web:9291cac6cfe569a946c50f"
+}; 
 
   const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
   const db = app.firestore();
-  const storage = app.storage();
+  const storage = firebase.storage();
 
   export { db, storage };
